853fa3426aa8f6d81abb80f579c21b9c073087a1
kentaro inomata <kentaro.inomata@unisys.co.jp>
2015-10-21 18:27:43 +0900

    first commit

---
+++ PhononApp/www/app.js
@@ -0,0 +1,17 @@
+﻿phonon.options({
+    navigator: {
+        defaultPage: 'login',
+        animatePages: true,
+        enableBrowserBackButton: true,
+        templateRootDirectory: './tpl'
+    },
+    i18n: null
+});
+
+var app = phonon.navigator();
+app.on({ page: 'login', preventClose: false, content: null });
+app.on({ page: 'main', preventClose: false, content: null });
+app.on({ page: 'pagetabs', preventClose: false, content: null });
+app.on({ page: 'home', preventClose: false, content: null });
+app.on({ page: 'pagetwo', preventClose: true, content: null });
+app.start();
