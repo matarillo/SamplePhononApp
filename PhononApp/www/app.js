973196ecd3d64741ccb71d0653db3bf434842e6b
kentaro inomata <kentaro.inomata@unisys.co.jp>
2015-10-23 12:26:19 +0900

    フォルダごとに機能分割。footer-barの実装を変更。

--- PhononApp/www/app.js
+++ PhononApp/www/app.js
@@ -4,16 +4,12 @@
             defaultPage: 'login',
             animatePages: true,
             enableBrowserBackButton: true,
-            templateRootDirectory: './tpl'
+            templateRootDirectory: ''
         },
         i18n: null
     });
 
     var app = phonon.navigator();
-    app.on({ page: 'login', preventClose: false, content: null });
-    app.on({ page: 'main', preventClose: false, content: null });
-    app.on({ page: 'pagetabs', preventClose: false, content: null });
-    app.on({ page: 'home', preventClose: false, content: null });
-    app.on({ page: 'pagetwo', preventClose: true, content: null });
+    app.on({ page: 'pagetwo', preventClose: true });
     app.start();
 });
